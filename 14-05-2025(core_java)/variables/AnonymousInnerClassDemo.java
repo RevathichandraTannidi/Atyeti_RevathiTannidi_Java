7d79ecbb5233dc158ed692f4a991ca163836b262
revathi-chandra23 <revathichandra2302@gmail.com>
2025-05-14 15:38:23 +0530

    variables

---
+++ 14-05-2025(core_java)/variables/AnonymousInnerClassDemo.java
@@ -0,0 +1,19 @@
+public class AnonymousInnerClassDemo {
+        public static void main(String[] args) {
+            final String greet = "Hello";
+            String name = "Revathi chandra";
+
+            Runnable greeter = new Runnable() {
+
+                @Override
+                public void run() {
+
+                    System.out.println(greet + ", " + name + "!");
+                }
+            };
+          //  name="raju"; it shows the error
+            greeter.run();
+        }
+    }
+
+
