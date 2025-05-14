adccab5a29246d3c41ca04c227169de3b8c6b70b
RevathichandraTannidi <revathi.tannidi@atyeti.com>
2025-05-14 09:31:54 +0530

    Delete src/main/java/dataTypes directory

--- src/main/java/dataTypes/DatatypeInteger.java
+++
@@ -1,8 +0,0 @@
-package dataTypes;
-
-public class DatatypeInteger {
-    public static void main(String[] args) {
-        int a=12;
-        System.out.println(a);
-    }
-}
--- src/main/java/dataTypes/nonPrimitive/Drawable.java
+++
@@ -1,5 +0,0 @@
-package dataTypes.nonPrimitive;
-
-public interface Drawable {
-    void draw();
-}
