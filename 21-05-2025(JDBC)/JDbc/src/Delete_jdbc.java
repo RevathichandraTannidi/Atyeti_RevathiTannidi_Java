68f6a43d53e73e5aff5de0739f6f9c3764b6f4f4
RevathichandraTannidi <revathi.tannidi@atyeti.com>
2025-05-21 17:27:21 +0530

    JDbc

---
+++ 21-05-2025(JDBC)/JDbc/src/Delete_jdbc.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Delete_jdbc {
+    public static void main(String[] args)  throws Exception{
+        String query = "delete from employees where id between 26 and 27";
+        Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student", "student");
+        System.out.println("Connection successful");
+        Statement st = myConn.createStatement();
+        st.execute(query);
+        System.out.println("Data deleted successfully");
+
+        System.out.println("Connection closed");
+
+    }
+}
