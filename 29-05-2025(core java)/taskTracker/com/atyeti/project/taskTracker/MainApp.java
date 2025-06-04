c783213d13f29698bc106e599ab8fe7c0a3f4af1
RevathichandraTannidi <revathi.tannidi@atyeti.com>
2025-06-04 12:10:40 +0530

    Task Tracker

--- 29-05-2025(core java)/taskTracker/com/atyeti/project/taskTracker/MainApp.java
+++ 29-05-2025(core java)/taskTracker/com/atyeti/project/taskTracker/MainApp.java
@@ -9,13 +9,11 @@ public class MainApp {
 
         manager.addTask("Finish report", "Work", "2025-06-10", "HIGH");
         manager.addTask("Doctor Appointment", "Health", "2025-06-05", "MEDIUM");
-        manager.addTask("Call John", "Personal", "2025-06-01", "LOW");
+        manager.addTask("rani", "Personal", "2025-06-01", "LOW");
 
         System.out.println(" All Tasks:");
         manager.getAllTasks().forEach(System.out::println);
-
         manager.markTaskCompleted(2);
-
         System.out.println("\n Completed Tasks:");
         manager.getCompletedTasks().forEach(System.out::println);
 
