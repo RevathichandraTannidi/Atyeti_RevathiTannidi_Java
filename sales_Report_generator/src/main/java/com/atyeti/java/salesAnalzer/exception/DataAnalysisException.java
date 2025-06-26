7f205c1a61b1d7bd4f3e8b623f880b79cefc42b1
RevathichandraTannidi <revathi.tannidi@atyeti.com>
2025-06-26 23:06:47 +0530

    sales report generator

---
+++ sales_Report_generator/src/main/java/com/atyeti/java/salesAnalzer/exception/DataAnalysisException.java
@@ -0,0 +1,8 @@
+package com.atyeti.java.salesAnalzer.exception;
+
+public class DataAnalysisException  extends  Exception{
+
+    public DataAnalysisException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
