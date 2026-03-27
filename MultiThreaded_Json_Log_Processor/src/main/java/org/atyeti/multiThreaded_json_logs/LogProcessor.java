114f9f3aa7433dc0d2b528c879a97f50634d29d8
revathi-chandra23 <revathi.tannidi@atyeti.com>
2026-03-27 17:30:01 +0530

    multi threaded json log processor

--- MultiThreaded_Json_Log_Processor/src/main/java/org/atyeti/multiThreaded_json_logs/LogProcessor.java
+++ MultiThreaded_Json_Log_Processor/src/main/java/org/atyeti/multiThreaded_json_logs/LogProcessor.java
@@ -8,19 +8,23 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class LogProcessor {
 
     public static void main(String[] args) {
 
-        String filePath = "C:\\Users\\RevathiTannidi\\OneDrive - Atyeti Inc\\Desktop\\Atyeti_RevathiTannidi_Java\\MultiThreaded_Json_Log_Processor\\src\\main\\resources\\logs.json";
+        String filePath = "src/main/resources/logs.json";
 
         int threads = Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors.newFixedThreadPool(threads);
 
         LogService service = new LogService();
         ObjectMapper mapper = new ObjectMapper();
+
+
+        List<JsonNode> updatedLogs = new CopyOnWriteArrayList<>();
 
         long start = System.currentTimeMillis();
 
@@ -36,6 +40,9 @@ public class LogProcessor {
                         LogEntry log = JsonUtil.parse(node);
 
                         service.process(log);
+                        JsonNode updated = service.updateLog(node);
+
+                        updatedLogs.add(updated);
 
                     } catch (Exception e) {
                         System.out.println("Failed to process node: " + node);
@@ -55,9 +62,30 @@ public class LogProcessor {
             e.printStackTrace();
         }
 
+
         service.printResults();
 
+
+        service.writeToJsonFile("src/main/resources/outputlogs.json");
+
+        //  Write updated logs file
+        writeUpdatedLogs(updatedLogs, "src/main/resources/updated_logs.json");
+
         long end = System.currentTimeMillis();
-        System.out.println("Time taken: " + (end - start) + " ms");
+        System.out.println("\n Time taken: " + (end - start) + " ms");
+    }
+
+
+    public static void writeUpdatedLogs(List<JsonNode> logs, String path) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writerWithDefaultPrettyPrinter()
+                    .writeValue(new File(path), logs);
+
+            System.out.println(" Updated logs written to: " + path);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
